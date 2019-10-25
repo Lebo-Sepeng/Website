20609b5fa10582725a69e32924183a26355074dd
Lebo-Sepeng <47536751+Lebo-Sepeng@users.noreply.github.com>
2019-10-25 23:35:22 +0200

    Add files via upload

---
+++ build/precache-manifest.5b9d5f2b2811c6cfe644735d1ace2f6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f579ac723d11b452fcadc4f60498c21b",
+    "url": "/IMY320/index.html"
+  },
+  {
+    "revision": "0c70a28810165181aa8e",
+    "url": "/IMY320/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "dbc4fedea98e376e6ea3",
+    "url": "/IMY320/static/css/main.75725f8b.chunk.css"
+  },
+  {
+    "revision": "0c70a28810165181aa8e",
+    "url": "/IMY320/static/js/2.c68b4d0e.chunk.js"
+  },
+  {
+    "revision": "dbc4fedea98e376e6ea3",
+    "url": "/IMY320/static/js/main.1b27f7b3.chunk.js"
+  },
+  {
+    "revision": "282c17f3a7178f45b5f1",
+    "url": "/IMY320/static/js/runtime-main.84aba88a.js"
+  }
+]);
